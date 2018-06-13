894a0e85f7f7b6ca3a21994a881a7a02b35006e3
hamity <hazemi94basic@gmail.com>
2018-06-14 01:07:18 +0900

    Playerの移動を実装

--- Invader/Assets/Scripts/Player/PlayerMover.cs
+++ Invader/Assets/Scripts/Player/PlayerMover.cs
@@ -3,14 +3,19 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class PlayerMover : MonoBehaviour {
+	[SerializeField] float speed = 10;
+	[SerializeField] float playerWidth = 4;
+	float maxWorldPosX = 0;
 
-	// Use this for initialization
-	void Start () {
-		
+	void Awake()
+	{
+		maxWorldPosX = Camera.main.ViewportToWorldPoint(new Vector3(1, 0, transform.position.z - Camera.main.transform.position.z)).x - playerWidth;
 	}
-	
-	// Update is called once per frame
-	void Update () {
-		
+
+	void Update()
+	{
+		float h = Input.GetAxis ("Horizontal");
+		float posX = Mathf.Clamp (transform.position.x + h * speed * Time.deltaTime, -maxWorldPosX, maxWorldPosX);
+		transform.position = new Vector3 (posX, transform.position.y, transform.position.z);
 	}
 }
