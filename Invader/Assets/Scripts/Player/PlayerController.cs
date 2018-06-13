76e5126e6131a231649d1440b74d2b73fcd55703
hamity <hazemi94basic@gmail.com>
2018-06-13 23:59:03 +0900

    違うResourceをインポートしていたため正しいものに変更, Playerのオブジェクトと最低限のクラスを作成

---
+++ Invader/Assets/Scripts/Player/PlayerController.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour {
+	[SerializeField]GameObject player;
+	[SerializeField]Vector3 startPos;
+	void OnEnable()
+	{
+		Instantiate (player, startPos, Quaternion.identity);
+	}
+}
