c524193c69e58abf7245e479bea3e0cc728294c7
hamity <hazemi94basic@gmail.com>
2018-06-14 23:51:23 +0900

    EnemyShotを実装

--- Invader/Assets/Scripts/Player/PlayerShot.cs
+++ Invader/Assets/Scripts/Player/PlayerShot.cs
@@ -3,14 +3,12 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class PlayerShot : MonoBehaviour {
+	[SerializeField]GameObject bullet;
 
-	// Use this for initialization
-	void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
+	void Update()
+	{
+		if (Input.GetKeyDown(KeyCode.Space)) {
+			Instantiate (bullet, transform.position, Quaternion.identity);		// TODO objectpoolを使用する
+		}
 	}
 }
